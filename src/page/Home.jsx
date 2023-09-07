435a1fb2121ab2295077a857de77d44f94c5e942
chint <chint@CHINU>
2023-09-07 13:44:08 +0530

    colorchange

---
+++ src/page/Home.jsx
@@ -0,0 +1,58 @@
+import React ,{useState,useEffect} from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import styled from '@emotion/styled';
+
+
+const Container = styled.div`
+display: flex;
+padding: 20px;
+align-items: center;
+gap:20px;
+flex-wrap: wrap;
+background-color: #A6FF96;
+`;
+const Boxe = styled.div`
+margin: 10px 0px;
+`;
+const Home = () => {
+    const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error('Error fetching data:', error));
+      console.log('posts', posts)
+  }, []);
+  return (
+    <Container>
+    {posts.map((item)=>(
+        <Boxe>
+            <Card sx={{height:300, backgroundColor:'#F3FDE7',borderRadius:5,width: 320}}>
+                <CardContent>
+                    <Typography gutterBottom variant='h5' component='div'>
+                        {item.title}
+                    </Typography>
+                    <Typography variant='body2' color='text.secondary'>
+                        {item.body}
+                    </Typography>
+                    
+                </CardContent>
+            </Card>
+        </Boxe>
+        ))}
+        
+    </Container>
+  )
+}
+
+export default Home
+
+
+
+
+
